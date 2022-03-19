68404c1cba268eeaf59daa2de9bf5ee1424df7e0
michaeldeven <michael.deven1999@gmail.com>
2022-03-19 12:12:04 +0700

    gass

---
+++ database/tms_db.sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2022 at 06:09 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tms_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_list`
+--
+
+CREATE TABLE `project_list` (
+  `id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(2) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `manager_id` int(30) NOT NULL,
+  `user_ids` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Task Management Karyawan', 'universalbpr.id', '+6948 8542 623', 'Emerald Avenue I, No 16-17,\n\nJl. Boulevard Bintaro Jaya,\n\nBintaro Jaya, Tangerang Selatan ', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_list`
+--
+
+CREATE TABLE `task_list` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1 = admin, 2 = staff',
+  `avatar` text NOT NULL DEFAULT 'no-image-available.png',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `avatar`, `date_created`) VALUES
+(1, 'Sandi', 'Simanjuntak', 'itdeveloper@universal.com', 'ac9b4e0b6a21758534db2a6324d34a54', 1, '1647603360_logobpr.png', '2022-03-18 18:36:40'),
+(2, 'Eveline ', 'Mary', 'Eveline@universal.com', '96e89a298e0a9f469b9ae458d6afae9f', 2, '1647603420_logobpr.png', '2022-03-18 18:37:21'),
+(3, 'Michael Deven', 'Bahari', 'deven@universal.com', '123bd00c3d8c9e24cb5c6a72b4eeaf6e', 3, '1647603480_-0f-DW52_400x400.jpg', '2022-03-18 18:38:15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_productivity`
+--
+
+CREATE TABLE `user_productivity` (
+  `id` int(30) NOT NULL,
+  `project_id` int(30) NOT NULL,
+  `task_id` int(30) NOT NULL,
+  `comment` text NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `user_id` int(30) NOT NULL,
+  `time_rendered` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `project_list`
+--
+ALTER TABLE `project_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_list`
+--
+ALTER TABLE `task_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `project_list`
+--
+ALTER TABLE `project_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `task_list`
+--
+ALTER TABLE `task_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `user_productivity`
+--
+ALTER TABLE `user_productivity`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
